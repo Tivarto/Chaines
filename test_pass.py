e6e7d901972143e30f1b7279c45aede075185bef
Robin <Robin@Robin-Laptop>
2016-01-06 15:27:24 +0100

    Partie 3. 1.1 1.2

--- test_pass.py
+++ test_pass.py
@@ -14,5 +14,55 @@ class TestPassword(unittest.TestCase):
         self.assertRaises(pwd.getNext("zzzzz"))
 
 
+
+    def test_hasSeries1(self):
+        self.assertEqual(pwd.hasSeries("abcd"), True)
+
+    def test_hasSeries2(self):
+        self.assertEqual(pwd.hasSeries("abcz"), True)
+
+    def test_hasSeries3(self):
+        self.assertEqual(pwd.hasSeries("zabc"), True)
+
+    def test_hasSeries4(self):
+        self.assertEqual(pwd.hasSeries("cbak"), False)
+
+    def test_hasSeries5(self):
+        self.assertEqual(pwd.hasSeries("azef"), False)
+
+
+    def test_hasNoBadChar1(self):
+        self.assertEqual(pwd.hasNoBadChar("aiol"), False)
+
+    def test_hasNoBadChar2(self):
+        self.assertEqual(pwd.hasNoBadChar("abcl"), False)
+
+    def test_hasNoBadChar3(self):
+        self.assertEqual(pwd.hasNoBadChar("abco"), False)
+
+    def test_hasNoBadChar4(self):
+        self.assertEqual(pwd.hasNoBadChar("abci"), False)
+
+    def test_hasNoBadChar5(self):
+        self.assertEqual(pwd.hasNoBadChar("azef"), True)
+
+
+    def test_hasTwoPair1(self):
+        self.assertEqual(pwd.hasNoBadChar("aazee"), True)
+
+    def test_hasTwoPair2(self):
+        self.assertEqual(pwd.hasNoBadChar("aaeez"), True)
+
+    def test_hasTwoPair3(self):
+        self.assertEqual(pwd.hasNoBadChar("aaeee"), True)
+
+    def test_hasTwoPair4(self):
+        self.assertEqual(pwd.hasNoBadChar("aaaze"), False)
+
+    def test_hasTwoPair5(self):
+        self.assertEqual(pwd.hasNoBadChar("aazed"), False)
+
+
+
 # Permet d'exécuter les tests si ce fichier est exécuté
 #unittest.main()
